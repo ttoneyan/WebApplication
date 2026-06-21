37d11e333ff1d6efd9e1cc8988aca079b2bb8292
Tsaghik <toneyantsaghik@gmail.com>
2026-06-21 23:13:21 +0400

    Added details

---
+++ Controllers/TermsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers;
+
+public class TermsController : Controller
+{
+    public IActionResult Terms()    
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
